22474a77fc44c28318e624673ac979a9460944b8
Matthew Harmon <harmonm96@gmail.com>
2022-03-16 19:11:18 +0000

    first run at db deployment file

---
+++ DB/Deployment.sql
@@ -0,0 +1,42 @@
+# create database
+create database BrawndoBros;
+
+# create each table
+create table Users (
+	userId int,
+	email varchar(255),
+	password varchar(255),
+	lastLogin varchar(255),
+);
+
+create table Plants (
+	userId int,
+	plantNickName varchar(255),
+	plantSpecies varchar(255),
+	plantImage varchar(255),
+	soilMoistureThreshold varchar(255),
+	soilMoistureCurrent varchar(255),
+	soilMoistureAction varchar(255),
+	soilMoistureDosage varchar(255),
+	soilTempThreshold varchar(255),
+	soilTempCurrent varchar(255),
+	soilTempAction varchar(255),
+	soilTempDosage varchar(255),
+	plantLightThreshold varchar(255),
+	plantLightCurrent varchar(255),
+	plantLightAction varchar(255),
+	plantLightDosage varchar(255)
+);
+
+create table PlantEvents (
+	plantId int,
+	eventType varchar(255),
+	eventValue varchar(255),
+	eventTime varchar(255)
+);
+
+create table ActionEvents (
+	plantId int,
+	actionType varchar(255),
+	actionTime varchar(255)
+);
