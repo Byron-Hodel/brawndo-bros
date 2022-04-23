43d2d56cae97b2d1fb099a79103088b68ffd8975
Byron-Hodel <bhodel@wccnet.edu>
2022-04-23 11:52:50 -0400

    made better

--- src/main.cpp
+++ src/main.cpp
@@ -27,6 +27,13 @@ size_t write_data(void *buffer, size_t size, size_t nmemb, void *userp) {
 	return size * nmemb;
 }
 
+typedef struct {
+	int32_t last_pump_time;
+	float soil_moisture;
+	float soil_temperature;
+	int32_t pump_state;
+} sensor_data_t;
+
 void log_to_server(const std::string str) {
 	CURL* curl = curl_easy_init();
 	curl_easy_setopt(curl, CURLOPT_URL, "http://brawndotest.mattmoose.net/api/events/plantId/1");
@@ -75,90 +82,88 @@ int main(int argc, char** argv) {
 	sleep(1);
 	tcflush(port_fd, TCIOFLUSH); // flush input and output to get rid of unwanted data present in buffer
 
-	std::vector<std::string> parameter_names;
-	std::string* str_values;
-	float* sensor_values;
+	//std::vector<std::string> parameter_names;
+	//std::string* str_values;
+	//float* sensor_values;
+	sensor_data_t avg = {0};
+	sensor_data_t prev = {-1};
+	bool pump_on = false;
 
 	int current_samples = 0;
 	std::string line;
 	while(1) {
 		char buffer[BUFFER_SIZE+1] = {0};
-		int32_t bytes_read = read(port_fd, buffer, BUFFER_SIZE);
-		for(int i = 0; i < bytes_read; i++) if(buffer[i] == ',') buffer[i] = ' ';
+		read(port_fd, buffer, BUFFER_SIZE);
 		line += std::string(buffer);
 
-		// read csv when line is complete
+		{
+			std::ifstream istream(file_path);
+			if(istream) {
+				istream.close();
+				log_to_csv("Pump_State,Soil_Moisture,Soil_Temp,Last_Pump_Time", file_path);
+			}
+			std::cout << "Pump_State,Soil_Moisture,Soil_Temp,Last_Pump_Time";
+		}
+
+
 		if(line[line.size()-1] == '\n') {
-			std::stringstream ss(line);
-			// make sure parameter names are read in
-			if(parameter_names.size() == 0) {
-				std::string parameter_str = "Date,Time,";
-				while(ss.peek() != -1) {
-					std::string param;
-					ss >> param;
-					if(param == "") break;
-					parameter_str += param + ',';
-					parameter_names.push_back(param);
-				}
-				// replace end comma with a new line character
-				parameter_str[parameter_str.size()-1] = '\n';
-				std::cout << parameter_str;
-				log_to_csv(parameter_str, file_path);
-				str_values = new std::string[parameter_names.size()];
-				sensor_values = new float[parameter_names.size()];
-				for(int i = 0; i < parameter_names.size(); i++) sensor_values[i] = 0;
-			}
-			// after parameter names are received, read actual data
-			else {
-				int index = 0;
-				while(ss.peek() != -1) {
-					std::string value_str;
-					ss >> value_str;
-					if(value_str == "") continue;
-					float value;
-					// check if it is a number (int or float), forces first read in value to be a string
-					if(std::stringstream(value_str) >> value && index != 0) {
-						sensor_values[index] += value;
-					}
-					else {
-						sensor_values[index] = -1;
-						str_values[index] = value_str;
-					}
-					index++;
-				}
-				current_samples++;
-			}
-			// if sample limit has been reached, average data and log to csv
+			sensor_data_t data;
+
+			sscanf(
+				line.c_str(), "%d,%f,%f,%d\n",
+				&data.last_pump_time, &data.soil_moisture,
+				&data.soil_temperature, &data.pump_state
+			);
+			avg.pump_state = data.pump_state;
+			avg.last_pump_time = data.last_pump_time;
+			avg.soil_moisture += data.soil_moisture / SAMPLES;
+			avg.soil_temperature += data.soil_moisture / SAMPLES;
+
+			current_samples++;
+
 			if(current_samples == SAMPLES) {
 				time_t t = time(0);
 				struct tm* p = localtime(&t);
 				char time_str[100];
 				char json_time_str[100];
 				strftime(time_str, 100, "%d/%m/%y,%H:%M", p);
-				strftime(json_time_str, 100, "%H:%M:%S", p);
-
-				std::string data_str = std::string(time_str) + ",";
-				char json_fmt[] = "{\"eventType\":\"data\",\"eventSubtype\":\"%s\",\"eventValue\":\"%s\",\"eventTime\":\"%s\"}";
-				//'{"eventType":"data","eventSubtype":"Soil Moisture","eventValue":"100","eventTime":"01:47:23"}'
-				for(int i = 0; i < parameter_names.size(); i++) {
-					std::string str;
-					if(sensor_values[i] == -1) str = str_values[i];
-					else str = std::to_string(sensor_values[i] / (float)SAMPLES);
-					sensor_values[i] = 0;
-					data_str += str;
-					if(i < parameter_names.size()-1) data_str += ",";
-					else data_str += '\n';
-
-					char json_str[500];
-					sprintf(json_str, json_fmt, parameter_names[i].c_str(), str.c_str(), json_time_str);
-					log_to_server(json_str);
-				}
-				std::cout << data_str;
-				log_to_csv(data_str, file_path);
+				strftime(json_time_str, 100, "%d/%m/%y %H:%M:%S", p);
 
 				current_samples = 0;
+				char str[1024];
+				sprintf(
+					str, "%s,%d,%.0f,%.0f,%d\n",
+					time_str, avg.pump_state, avg.soil_moisture,
+					avg.soil_temperature, avg.last_pump_time
+				);
+				log_to_csv(str, file_path);
+
+				if(prev.pump_state == -1) continue;
+
+				const char json_fmt[] = "{"
+					"\"eventType\":\"data\","
+					"\"eventSubtype\":\"%s\","
+					"\"eventValue\":\"%f\","
+					"\"eventTime\":\"%s\""
+					"}";
+
+				// send pump event to server when pump has changed state
+				if(data.pump_state != prev.pump_state) {
+					pump_on = true;
+					sprintf(str, json_fmt, "pumpState", (float)data.pump_state, json_time_str);
+					log_to_server(std::string(str));
+				}
+
+				if(data.soil_moisture != prev.soil_moisture) {
+					sprintf(str, json_fmt, "soilMoisture", avg.soil_moisture, json_time_str);
+					log_to_server(std::string(str));
+				}
+				if(data.soil_temperature != prev.soil_temperature) {
+					sprintf(str, json_fmt, "soilTemperature", avg.soil_temperature, json_time_str);
+					log_to_server(std::string(str));
+				}
+				prev = data;
 			}
-			line.clear();
 		}
 	}
 	return 0;
